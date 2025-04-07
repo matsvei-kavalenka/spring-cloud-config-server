f48901e9e8fa4c391b5f5f9416c7dc24c4f7b4b9
matsvei-kavalenka <matsvei.kavalenka@gmail.com>
2025-04-07 17:43:44 +0300

    Finished first setup

--- src/main/java/lt/vilniustech/mkavalenka/cloud_config_server/CloudConfigServerApplication.java
+++ src/main/java/lt/vilniustech/mkavalenka/cloud_config_server/CloudConfigServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class CloudConfigServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CloudConfigServerApplication.class, args);
+	public static void main(String[] arguments) {
+		SpringApplication.run(CloudConfigServerApplication.class, arguments);
 	}
-
 }
